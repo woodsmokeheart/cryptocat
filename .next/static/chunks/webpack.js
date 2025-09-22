90ea97ded6d4a9381ae4c7b36b752bc67834959c
Denis Kukobin <alterblckwd@gmail.com>
2025-09-22 13:41:16 +0300

    Center icons in slider control buttons
    - Updated .swiperSlideControls to use flexbox for perfect centering
    - Updated .swiperSlideControlsPlayPause to use flexbox for perfect centering
    - Icons now perfectly centered both horizontally and vertically
    - Improved visual consistency across all control buttons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d7197fe56cbf25da"; }
+/******/ 		__webpack_require__.h = function() { return "7350925c9ad5410a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
