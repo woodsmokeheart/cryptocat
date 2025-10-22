f4567bb97c8c2bed430dce449cbe90dd819cb25a
Denis Kukobin <alterblckwd@gmail.com>
2025-10-22 17:49:27 +0300

    feat: Reorder contact blocks and add YouTube
    - Reorder contact blocks: Telegram → Referral → TradingView → YouTube → Email
    - Add new YouTube contact block with red YouTube icon
    - Add YouTube-specific styling with red background
    - Import FaYoutube icon for YouTube block
    - Maintain consistent styling with other contact blocks

--- src/components/ContactContent/ContactContent.tsx
+++ src/components/ContactContent/ContactContent.tsx
@@ -1,30 +1,16 @@
-"use client"
+"use client";
 
-import React from 'react'
-import styles from './ContactContent.module.css'
-import ContactCard from './ContactCard'
-import { MdOutlineEmail } from 'react-icons/md'
-import { FaTelegramPlane, FaLink } from 'react-icons/fa'
-import { SiTradingview } from 'react-icons/si'
-import { FiExternalLink } from 'react-icons/fi'
+import React from "react";
+import styles from "./ContactContent.module.css";
+import ContactCard from "./ContactCard";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaTelegramPlane, FaLink, FaYoutube } from "react-icons/fa";
+import { SiTradingview } from "react-icons/si";
+import { FiExternalLink } from "react-icons/fi";
 
 const ContactContent: React.FC = () => {
   return (
     <div className={styles.container}>
-      {/* Email */}
-      <ContactCard variant="email">
-        <div className={styles.headerRow}>
-          <MdOutlineEmail color="#FF7A3D" size={22} />
-          <h4 className={styles.cardTitle}>Email</h4>
-        </div>
-        <div className={styles.linkRowCenterMobile}>
-          <a className={styles.pill}
-             href="mailto:cryptocatagency@gmail.com">
-            cryptocatagency@gmail.com
-          </a>
-        </div>
-      </ContactCard>
-
       {/* Telegram */}
       <ContactCard className={styles.cardTelegram} variant="telegram">
         <div className={styles.headerRow}>
@@ -32,10 +18,12 @@ const ContactContent: React.FC = () => {
           <h4 className={styles.cardTitle}>Telegram</h4>
         </div>
         <div className={styles.linkRowCenterMobile}>
-          <a className={`${styles.pill} ${styles.pillTelegram}`}
-             href="https://t.me/cryptocat_manager"
-             target="_blank"
-             rel="noopener noreferrer">
+          <a
+            className={`${styles.pill} ${styles.pillTelegram}`}
+            href="https://t.me/cryptocat_manager"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             cryptocat_manager <FiExternalLink size={16} />
           </a>
         </div>
@@ -48,10 +36,12 @@ const ContactContent: React.FC = () => {
           <h4 className={styles.cardTitle}>Реферальная программа Bingx</h4>
         </div>
         <div className={styles.linkRowCenterMobile}>
-          <a className={styles.pill}
-             href="https://bingx.com/invite/MT6UVTEUZ"
-             target="_blank"
-             rel="noopener noreferrer">
+          <a
+            className={styles.pill}
+            href="https://bingx.com/invite/MT6UVTEUZ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Реферальная ссылка <FiExternalLink size={16} />
           </a>
         </div>
@@ -64,30 +54,65 @@ const ContactContent: React.FC = () => {
           <h4 className={styles.cardTitle}>TradingView</h4>
         </div>
         <div className={styles.chipsRow}>
-          <a className={styles.chip}
-             href="https://ru.tradingview.com/u/cryptocatagency"
-             target="_blank"
-             rel="noopener noreferrer">
-            Основной канал <FiExternalLink size={14} />
+          <a
+            className={styles.chip}
+            href="https://ru.tradingview.com/u/cryptocatagency"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            cryptocatagency <FiExternalLink size={14} />
           </a>
-          <a className={styles.chip}
-             href="https://ru.tradingview.com/u/CryptoCat_Squad"
-             target="_blank"
-             rel="noopener noreferrer">
-            Вторичный канал <FiExternalLink size={14} />
+          <a
+            className={styles.chip}
+            href="https://ru.tradingview.com/u/CryptoCat_Squad"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CryptoCat_Squad <FiExternalLink size={14} />
           </a>
-          <a className={styles.chip}
-             href="https://ru.tradingview.com/u/cryptocatagency_kitty"
-             target="_blank"
-             rel="noopener noreferrer">
-            Третичный канал <FiExternalLink size={14} />
+          <a
+            className={styles.chip}
+            href="https://ru.tradingview.com/u/cryptocatagency_kitty"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            cryptocatagency_kitty <FiExternalLink size={14} />
+          </a>
+        </div>
+      </ContactCard>
+
+      {/* YouTube */}
+      <ContactCard variant="youtube">
+        <div className={styles.headerRow}>
+          <FaYoutube color="#FF0000" size={20} />
+          <h4 className={styles.cardTitle}>YouTube</h4>
+        </div>
+        <div className={styles.linkRowCenterMobile}>
+          <a
+            className={`${styles.pill} ${styles.pillYoutube}`}
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            YouTube канал <FiExternalLink size={16} />
+          </a>
+        </div>
+      </ContactCard>
+
+      {/* Email */}
+      <ContactCard variant="email">
+        <div className={styles.headerRow}>
+          <MdOutlineEmail color="#FF7A3D" size={22} />
+          <h4 className={styles.cardTitle}>Email</h4>
+        </div>
+        <div className={styles.linkRowCenterMobile}>
+          <a className={styles.pill} href="mailto:cryptocatagency@gmail.com">
+            cryptocatagency@gmail.com
           </a>
         </div>
       </ContactCard>
     </div>
-  )
-}
+  );
+};
 
-export default ContactContent
-
-
+export default ContactContent;
